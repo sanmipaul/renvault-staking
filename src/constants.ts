f40f33136f1cced4708f4759f590921ef04316ff
sanmipaul <sanmipaul1@gmail.com>
2026-04-13 10:22:25 +0100

    feat: convert constants.js to TypeScript with JSDoc comments

---
+++ src/constants.ts
@@ -0,0 +1,14 @@
+/** Minimum stake amount in micro-STX (1 STX) */
+export const MIN_STAKE_AMOUNT = 1_000_000;
+
+/** Maximum stake amount in micro-STX (1,000,000 STX) */
+export const MAX_STAKE_AMOUNT = 1_000_000_000_000;
+
+/** Default lock period in Stacks blocks (~1 day) */
+export const DEFAULT_LOCK_PERIOD = 144;
+
+/** Denominator used when expressing reward rates as integer basis points */
+export const REWARD_RATE_DENOMINATOR = 10_000;
+
+/** On-chain staking contract name */
+export const CONTRACT_NAME = 'staking';
